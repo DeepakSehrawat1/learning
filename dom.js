cf1781583c475ea388152e1536b310c0ef3c582d
DeepakSehrawat1 <deepaksehrawat150@gmail.com>
2023-11-24 10:05:26 +0530

    changing

--- dom.js
+++ dom.js
@@ -1,10 +1,17 @@
-console.log("hello");
+/*console.log("hello");
 console.log(document);
 console.log(document.body);
 console.log(document.URL);
 console.log(document.all);
 
 document.title = "new title";
+*/
 
 var heade = document.getElementById("main-header");
 heade.style.border = "solid 2px black";
+
+var items = document.getElementsByClassName("list-group-item");
+items[2].style.background = "green";
+for (let i = 0; i < items.length; i++) {
+  items[i].style.fontWeight = "bold";
+}
