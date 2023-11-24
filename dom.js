7a2960c58a74d4aa1c95e091989894c034c66308
DeepakSehrawat1 <deepaksehrawat150@gmail.com>
2023-11-24 09:27:42 +0530

    starting javascript

---
+++ dom.js
@@ -0,0 +1,10 @@
+console.log("hello");
+console.log(document);
+console.log(document.body);
+console.log(document.URL);
+console.log(document.all);
+
+document.title = "new title";
+
+var heade = document.getElementById("main-header");
+heade.style.border = "solid 2px black";
